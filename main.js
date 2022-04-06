c9bc413b0eb2643a3d66c16c207793b1f92e8f6c
LK4268 <46650031+LK4268@users.noreply.github.com>
2022-04-06 15:45:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 const div = document.getElementById('adinfo');
 var domainname = window.location.host;
 div.innerHTML = '<span style="background-color: lime">Replacement:'+domainname+'</span>';
+
+function httpGet(theUrl)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            createDiv(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", theUrl, false);
+    xmlhttp.send();    
+}
