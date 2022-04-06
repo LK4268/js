c7e58c91e4eb30eabf57a3b83b941655d32a19b8
LK4268 <46650031+LK4268@users.noreply.github.com>
2022-04-06 21:41:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-const divadd = document.getElementById('adinfo');
 var domainname = window.location.host;
-divadd.innerHTML = '<a href="http://download.viettechkey.com/file_download/FirefoxSetup.rar"><img src="http://img.viettechkey.com/icon_download/ff.png" height="40"></a>
-<a href="http://download.viettechkey.com/file_download/ChromeSetup.rar"><img src="http://img.viettechkey.com/icon_download/chrome.png" height="40"></a>
-<a href="http://download.viettechkey.com/file_download/IDM.zip"><img src="http://img.viettechkey.com/icon_download/idm.png" height="40"></a>
-<a href="http://download.viettechkey.com/file_download/unikey42RC4.zip"><img src="http://img.viettechkey.com/icon_download/unikey.jpg" height="40"></a>
-<a href="http://download.viettechkey.com/file_download/winrar.rar"><img src="http://img.viettechkey.com/icon_download/winrar.png" height="40"></a>
-<a href="http://download.cnet.com/FLV-Player-Free/3000-13632_4-10909512.html"><img src="http://img.viettechkey.com/icon_download/flv.jpg" height="40"></a>
-<a href="http://download.viettechkey.com/file_download/TeamViewerPortable.zip"><img src="http://img.viettechkey.com/icon_download/teamviewer.png" height="40"></a>
-<a href="http://download.viettechkey.com/file_download/FoxitReader.rar"><img src="http://img.viettechkey.com/icon_download/pdf.png" height="40"></a>';
+const divadd = document.getElementById('adinfo');
+divadd.innerHTML = '<a href="http://download.viettechkey.com/file_download/FirefoxSetup.rar"><img src="http://img.viettechkey.com/icon_download/ff.png" height="40"></a><a href="http://download.viettechkey.com/file_download/ChromeSetup.rar"><img src="http://img.viettechkey.com/icon_download/chrome.png" height="40"></a><a href="http://download.viettechkey.com/file_download/IDM.zip"><img src="http://img.viettechkey.com/icon_download/idm.png" height="40"></a><a href="http://download.viettechkey.com/file_download/unikey42RC4.zip"><img src="http://img.viettechkey.com/icon_download/unikey.jpg" height="40"></a><a href="http://download.viettechkey.com/file_download/winrar.rar"><img src="http://img.viettechkey.com/icon_download/winrar.png" height="40"></a><a href="http://download.cnet.com/FLV-Player-Free/3000-13632_4-10909512.html"><img src="http://img.viettechkey.com/icon_download/flv.jpg" height="40"></a><a href="http://download.viettechkey.com/file_download/TeamViewerPortable.zip"><img src="http://img.viettechkey.com/icon_download/teamviewer.png" height="40"></a><a href="http://download.viettechkey.com/file_download/FoxitReader.rar"><img src="http://img.viettechkey.com/icon_download/pdf.png" height="40"></a>';
 
 function httpGet(theUrl)
 {
